200f8be53d03fe23a5cb2d270c085060f7bacbec
Usama Fazal <usamafazal222@gmail.com>
2026-06-06 15:15:01 +0500

    refactor: fixed a bug in app.js category routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
+import categoryRoutes from "./routes/category.routes.js";
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/categories", categoryRoutes);
 
 export default app;

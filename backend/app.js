e065d01f0335e6e820c352068f514609a9b44c63
Usama Fazal <usamafazal222@gmail.com>
2026-06-07 11:30:21 +0500

    refactor: added cors middleware

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,18 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.routes.js";
 import categoryRoutes from "./routes/category.routes.js";
+import cors from "cors";
 
 const app = express();
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/categories", categoryRoutes);
